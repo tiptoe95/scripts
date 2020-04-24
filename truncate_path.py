37c82a9676329afed1e4706e615b41cab2bfe820
kyle <hoppnstopp@protonmail.com>
2020-04-24 10:00:38 -0500

    FIXED: file paths were being exectued when passed to prompt generator

--- truncate_path.py
+++ truncate_path.py
@@ -2,13 +2,14 @@
 Takes a file path and a home path and returns the path in a format appropriate for the bash prompt.
 """
 import sys
+debug_file = '~/debug'
 
 
 path = sys.argv[1]
 home = sys.argv[2]
 dirs = path.replace(home, '~')
-
 dirs = dirs.split('/')
+
 if len(dirs) > 3:
     print(('/').join(d[:4]+' \u05bb' if len(d) > 4 else d for d in dirs[:-2]) + 
         '/' +
